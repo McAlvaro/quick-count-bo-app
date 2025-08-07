a775f7a22c682ef48c776866c270f0ef43d0e7fb
McAlvaro <mc.alv641@gmail.com>
2025-08-07 01:47:37 -0400

    feat: add election system database structure

---
+++ database/migrations/2025_08_07_053224_create_tables_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tables', function (Blueprint $table) {
+            $table->id();
+            $table->integer(column: 'number');
+            $table->foreignId(column: 'precinct_id')->constrained('precincts')->onDelete('cascade');
+            $table->integer(column: 'null_votes')->default(value: 0);
+            $table->integer(column: 'blank_votes')->default(value: 0);
+            $table->string(column: 'act_path')->nullable();
+            $table->foreignId(column: 'registered_by')->constrained('users')->onDelete('cascade');
+            $table->timestamps('registered_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tables');
+    }
+};
